31e1c186b1374b1dbfffbb25b97004dded3bb499
rolandKoczan <roland.koczan@gmail.com>
2021-05-04 21:34:47 +0200

    Added game menu component for testing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import PlayersBoard from "./components/PlayersBoard/PlayersBoard";
-import PlayArea from "./components/PlayArea/PlayArea";
+import PlayersBoard from "./components/PlayersBoard/PlayersBoard"
+import PlayArea from "./components/PlayArea/PlayArea"
+import GameMenu from "./components/GameMenu/GameMenu"
 
-import './App.css';
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-        <PlayArea/>
-        <PlayersBoard/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <GameMenu />
+            {/* <PlayArea />
+            <PlayersBoard /> */}
+        </div>
+    )
 }
 
-export default App;
+export default App
