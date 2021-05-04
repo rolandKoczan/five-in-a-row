a7104ee017cc382d54f81fdd3bb337f5948a9a65
rolandKoczan <roland.koczan@gmail.com>
2021-05-04 11:37:34 +0200

    Implemented higlighting in case of win

--- src/components/PlayArea/PlayArea.js
+++ src/components/PlayArea/PlayArea.js
@@ -1,14 +1,15 @@
 import { useSelector, useDispatch } from "react-redux"
 
-import { setSingleElement } from "../../store/board"
-import { nextPlayer } from "../../store/players"
+import { setSingleElement, setWinnerCoordinates } from "../../store/board"
+import { nextPlayer, setWinner } from "../../store/players"
+import { checkIsWinner } from "../../utility/gameLogic"
 
 import SingleSquare from "../SingleSquare/SingleSquare"
 
 import "./PlayArea.css"
 
 const PlayArea = () => {
-    const tiles = useSelector((state) => state.board.tiles)
+    const { tiles, winnerCoordinates } = useSelector((state) => state.board)
     const { players, currentPlayer } = useSelector((state) => state.players)
 
     const dispatch = useDispatch()
@@ -16,9 +17,14 @@ const PlayArea = () => {
     const returnPlayer = (id) => players.find((player) => player.id === id)
 
     const onTileClicked = (player, y, x, playerId) => {
-        if (player === undefined) {
+        const winnerCoordinates = checkIsWinner(tiles, playerId, y, x)
+        if (player === undefined && !winnerCoordinates) {
             dispatch(setSingleElement({ y, x, playerId }))
             dispatch(nextPlayer())
+        } else if (winnerCoordinates) {
+            dispatch(setWinnerCoordinates(winnerCoordinates))
+            dispatch(setSingleElement({ y, x, playerId }))
+            dispatch(setWinner({ playerId }))
         }
     }
 
@@ -27,13 +33,19 @@ const PlayArea = () => {
             {tiles.map((row, y) =>
                 row.map((playerId, x) => {
                     const player = returnPlayer(playerId)
+                    let isWinnerTile = null
+                    if (winnerCoordinates) {
+                        isWinnerTile = winnerCoordinates.find(
+                            (coordinates) =>
+                                coordinates.x === x && coordinates.y === y
+                        )
+                    }
                     return (
                         <SingleSquare
                             key={`x:${x},y${y}`}
-                            x={x}
-                            y={y}
                             symbol={player ? player.symbol : ""}
                             color={player ? player.color : "#fff"}
+                            isWinnerTile={isWinnerTile}
                             onTileClicked={() =>
                                 onTileClicked(player, y, x, currentPlayer)
                             }
